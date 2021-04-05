8cc873162a541a70e6c47b375289f5210d61db83
peter <peter@retep.org>
2021-04-05 18:41:06 +0100

    Initial commit after moving out of departures8bit

--- charset.asm
+++ charset.asm
@@ -2,6 +2,20 @@
 ; Character set for ascii codes 32-127
 ;
 ; This is formed of 8 bytes per character
+;
+;  Copyright 2021 Peter Mount
+;
+;  Licensed under the Apache License, Version 2.0 (the "License");
+;  you may not use this file except in compliance with the License.
+;  You may obtain a copy of the License at
+;
+;     http://www.apache.org/licenses/LICENSE-2.0
+;
+;  Unless required by applicable law or agreed to in writing, software
+;  distributed under the License is distributed on an "AS IS" BASIS,
+;  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+;  See the License for the specific language governing permissions and
+;  limitations under the License.
 ; **********************************************************************
 .charset
     EQUB &00  ;00000000   ........   &20  32 - ' '
